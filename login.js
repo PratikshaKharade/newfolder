7031c5d673f522f33f408b9857bb05e301388fc7
unknown <pratikshakharade984@gmail.com>
2023-01-30 07:21:19 +0000

    second commit

--- login.js
+++ login.js
@@ -3,3 +3,10 @@ console.log('hii bhagaPratikshaKharadePratikshaKharade')
 //PratikshaKharade
 
 //git config --global user.email "pratikshakharade984@gmail.com"
+
+
+console.log('pratiksha kharade')
+
+
+
+
